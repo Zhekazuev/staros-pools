095e10f69771d2ab5a77e091ffe91b717a813810
Eugene Zuev <zhekazuev@gmail.com>
2020-10-30 16:14:03 +0300

    Add config.example.py

---
+++ config.example.py
@@ -0,0 +1,9 @@
+class StarOS:
+    user = 'user'
+    password = 'pass'
+
+    hosts = {
+"region1": [{"region": "region1", "hostname": "host1", "host": "1.1.1.1"},
+            {"region": "region1", "hostname": "host2", "host": "1.1.1.2"}],
+"region2": [{"region": "region2", "hostname": "host1", "host": "1.1.2.1"},
+            {"region": "region2", "hostname": "host2", "host": "1.1.2.2"}]}
